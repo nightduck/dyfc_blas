4bd1a048a77d309c98d51d74ff8c2e68558c391c
Oren Bell <virtualoren@gmail.com>
2025-03-27 15:58:28 -0500

    Hyper-parallelized version of benchmark.sh working, but csv lines are out of order
    Signed-off-by: Oren Bell <virtualoren@gmail.com>

--- benchmark.sh
+++ benchmark.sh
@@ -8,9 +8,11 @@
 
 # Parse command line flags
 PARALLEL=false
-while getopts "p" opt; do
+PARALLEL_JOBS=$(nproc)
+while getopts "p:" opt; do
   case $opt in
-  p) PARALLEL=true ;;
+  p) PARALLEL=true
+     PARALLEL_JOBS=${OPTARG:-$(nproc)} ;;
   \?)
     echo "Invalid option -$OPTARG" >&2
     exit 1
@@ -19,8 +21,12 @@ while getopts "p" opt; do
 done
 shift $((OPTIND - 1))
 
+# Clean up temp directory, in case a prior run of this script was interrupted
+rm -rf "$WORKSPACE_DIR/temp"
+
 # Directory containing the tests
-TEST_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/test" && pwd)"
+# TEST_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/test" && pwd)"
+WORKSPACE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Check if Vitis is sourced
 if ! command -v v++ &>/dev/null; then
@@ -31,19 +37,19 @@ fi
 # Get the list of command line arguments
 if [ "$#" -eq 0 ]; then
   # No arguments passed, use the default test list
-  TEST_LIST="daxpy ddot dgemv_rm dgemv_cm prefixsum dgemm_rc"
-  TEST_LIST=$(echo "$TEST_LIST" | sed "s~[^ ]*~$TEST_DIR/&~g")
+  TEST_LIST="daxpy ddot dgemv_rm dgemv_cm prefixsum dgemm_rcr dgemm_rcc"
+  TEST_LIST=$(echo "$TEST_LIST" | sed "s~[^ ]*~$WORKSPACE_DIR/test/&~g")
 else
-  # Arguments passed, verify they match subdirectories in TEST_DIR
+  # Arguments passed, verify they match subdirectories in test/
   TEST_LIST=""
   SIZE_LIST=""
   for ARG in "$@"; do
     IFS=':' read -r DIR N M K <<<"$ARG"
-    if [ -d "$TEST_DIR/$DIR" ]; then
-      TEST_LIST="$TEST_LIST $TEST_DIR/$DIR"
+    if [ -d "$WORKSPACE_DIR/test/$DIR" ]; then
+      TEST_LIST="$TEST_LIST $WORKSPACE_DIR/test/$DIR"
       SIZE_LIST="$SIZE_LIST ${N}:${M}:${K}"
     else
-      echo "!! Directory $DIR does not exist in $TEST_DIR."
+      echo "!! Directory $DIR does not exist in $WORKSPACE_DIR/test."
     fi
   done
 fi
@@ -53,12 +59,136 @@ run_test() {
   local TEST="$1"
   local SIZE="$2"
 
+  # Make temp directory for the test
+  local TEMP_DIR="$WORKSPACE_DIR/temp/$TEST:$SIZE"
+  mkdir -p "$TEMP_DIR"
+
+  # Copy contents of test to the temp directory
+  cp -r "$WORKSPACE_DIR/test/$TEST/"* "$TEMP_DIR/"
+
+  local CONFIG_FILE="$TEMP_DIR/hls_config.cfg"
+  
+  # Clean up hls_config.cfg in case a prior run of this script was interrupted
+  if tail -n 1 "$CONFIG_FILE" | grep -q "syn.cflags=-D dimK"; then
+    sed -i '$d' "$CONFIG_FILE"
+  fi
+  if tail -n 1 "$CONFIG_FILE" | grep -q "tb.cflags=-D dimK"; then
+    sed -i '$d' "$CONFIG_FILE"
+  fi
+
+  # Edit hls_config.cfg with the current m,n,k values in the format
+  echo "tb.cflags=-D dimK=$K -D dimM=$M -D dimN=$N" >>"$CONFIG_FILE"
+  echo "syn.cflags=-D dimK=$K -D dimM=$M -D dimN=$N" >>"$CONFIG_FILE"
+
+  # TODO: Have a flag that lets each size permutation of each test run in parallel. They would go into
+  # different folders, eg build64:64:64, build64:64:128, etc. This line (which is 99% of the compute time)
+  # would need to go into a different bash function. You would also need to deal with each size permutation
+  # writing to the same csv file. Maybe have the lines below return the line they're contributing to the csv
+  # file and this function will aggregate them. It can submit multiple jobs to the background, one for each
+  # size permutation. It will wait on the output of each job in order to aggregate the results. Meanwhile,
+  # other tests that also called this function would be submitting their own jobs to run. Something in this script
+  # has to set a limit on how many jobs can run in parallel and coordinate everything.
+
+  # Run the v++ command
+  (cd "$TEMP_DIR" && v++ -c --mode hls --config "$CONFIG_FILE" --work_dir $TEMP_DIR/build >/dev/null 2>&1)
+
+  # Check if the command was successful
+  if [ $? -ne 0 ]; then
+    echo "  !! $TEST_NAME failed"
+  fi
+
+  # Extract the relevant information from csynth.rpt
+  CSYNTH_RPT="$TEMP_DIR/build/hls/syn/report/csynth.rpt"
+  if [ -f "$CSYNTH_RPT" ]; then
+    read latency_cycles latency_ns bram dsp ff lut uram issue_type < <(awk '
+    BEGIN {
+      hyphen_rows = 0;
+      reading_table = 0;
+    }
+    {
+      if ($0 ~ /\+-+\+/) {
+        hyphen_rows++;
+        if (reading_table == 1) {
+          reading_table = 0;
+          print latency_cycles, latency_ns, bram[1], dsp[1], ff[1], lut[1], uram[1], "";
+          exit;
+        }
+      }
+      if (hyphen_rows == 2 && reading_table == 0) {
+        reading_table = 1;
+        getline;
+        split($0, columns, "|");
+        latency_cycles = columns[5];
+        latency_ns = columns[6];
+        split(columns[11], bram, " ");
+        split(columns[12], dsp, " ");
+        split(columns[13], ff, " ");
+        split(columns[14], lut, " ");
+        split(columns[15], uram, " ");
+      }
+      if (hyphen_rows == 2 && reading_table == 1) {
+        split($0, columns, "|");
+        issue_type = columns[3];
+        if (issue_type ~ /II/) {
+          print latency_cycles, latency_ns, bram[1], dsp[1], ff[1], lut[1], uram[1], "II";
+          exit;
+        }
+      }
+    }
+    ' "$CSYNTH_RPT")
+
+    # Print the extracted values
+    if [ ! -z "$issue_type" ]; then
+      echo "  Issue Type: $issue_type"
+    else
+      echo "  Latency (cycles): $latency_cycles"
+      echo "  Latency (ns): $latency_ns"
+      echo "  BRAM: $bram"
+      echo "  DSP: $dsp"
+      echo "  FF: $ff"
+      echo "  LUT: $lut"
+      echo "  URAM: $uram"
+    fi
+
+    # Write the extracted values to the benchmark file
+    if ([ -z "$issue_type" ]); then
+      echo "$M,$N,$K,$latency_cycles,$latency_ns,$bram,$dsp,$ff,$lut,$uram" >>"$WORKSPACE_DIR"/benchmarks/"$TEST_NAME".csv
+    else
+      echo "$M,$N,$K,$issue_type" >>"$WORKSPACE_DIR"/benchmarks/"$TEST_NAME".csv
+    fi
+  else
+    echo "!! csynth.rpt not found for $TEST_NAME."
+  fi
+
+  # Remove the last two lines from the config file that we added to specify the m,n,k values
+  if tail -n 1 "$CONFIG_FILE" | grep -q "syn.cflags=-D dimK=$K -D dimM=$M -D dimN=$N"; then
+    sed -i '$d' "$CONFIG_FILE"
+  fi
+  if tail -n 1 "$CONFIG_FILE" | grep -q "tb.cflags=-D dimK=$K -D dimM=$M -D dimN=$N"; then
+    sed -i '$d' "$CONFIG_FILE"
+  fi
+
+  # Remove the temp directory
+  rm -rf "$TEMP_DIR"
+}
+
+# Create benchmarks directory if it doesn't exist, and clear it if it does
+mkdir -p "$WORKSPACE_DIR"/benchmarks
+
+# Iterate over all the tests in the test directory
+TEST_ARRAY=($TEST_LIST)
+SIZE_ARRAY=($SIZE_LIST)
+
+# Run tests in parallel
+for i in "${!TEST_ARRAY[@]}"; do
+  TEST="${TEST_ARRAY[$i]}"
+  SIZE="${SIZE_ARRAY[$i]}"
+
   if [ ! -d "$TEST" ]; then
     return
   fi
 
-  local TEST_NAME=$(basename "$TEST")
-  local CONFIG_FILE="$TEST/hls_config.cfg"
+  TEST_NAME=$(basename "$TEST")
 
   IFS=':' read -r N_MAX M_MAX K_MAX <<<"$SIZE"
 
@@ -95,7 +225,7 @@ run_test() {
   fi
 
   # Create csv file to store the results
-  echo "M,N,K,Latency (cycles),Latency (ns),BRAM,DSP,FF,LUT,URAM" >"$TEST_DIR"/../benchmarks/"$TEST_NAME".csv
+  echo "M,N,K,Latency (cycles),Latency (ns),BRAM,DSP,FF,LUT,URAM" >"$WORKSPACE_DIR"/benchmarks/"$TEST_NAME".csv
 
   M=$M_MIN
   while [ "$M" -le "$M_MAX" ]; do
@@ -105,104 +235,14 @@ run_test() {
       while [ "$K" -le "$K_MAX" ]; do
         echo "$TEST_NAME: M=$M, N=$N, K=$K"
 
-        # Clean up hls_config.cfg in case a prior run of this script was interrupted
-        if tail -n 1 "$CONFIG_FILE" | grep -q "syn.cflags=-D dimK"; then
-          sed -i '$d' "$CONFIG_FILE"
-        fi
-        if tail -n 1 "$CONFIG_FILE" | grep -q "tb.cflags=-D dimK"; then
-          sed -i '$d' "$CONFIG_FILE"
-        fi
 
-        # Edit hls_config.cfg with the current m,n,k values in the format
-        echo "tb.cflags=-D dimK=$K -D dimM=$M -D dimN=$N" >>"$CONFIG_FILE"
-        echo "syn.cflags=-D dimK=$K -D dimM=$M -D dimN=$N" >>"$CONFIG_FILE"
-
-        # TODO: Have a flag that lets each size permutation of each test run in parallel. They would go into
-        # different folders, eg build64:64:64, build64:64:128, etc. This line (which is 99% of the compute time)
-        # would need to go into a different bash function. You would also need to deal with each size permutation
-        # writing to the same csv file. Maybe have the lines below return the line they're contributing to the csv
-        # file and this function will aggregate them. It can submit multiple jobs to the background, one for each
-        # size permutation. It will wait on the output of each job in order to aggregate the results. Meanwhile,
-        # other tests that also called this function would be submitting their own jobs to run. Something in this script
-        # has to set a limit on how many jobs can run in parallel and coordinate everything.
-
-        # Run the v++ command
-        (cd "$TEST" && v++ -c --mode hls --config "$CONFIG_FILE" --work_dir $TEST/build >/dev/null 2>&1)
-
-        # Check if the command was successful
-        if [ $? -ne 0 ]; then
-          echo "  !! $TEST_NAME failed"
-        fi
-
-        # Extract the relevant information from csynth.rpt
-        CSYNTH_RPT="$TEST/build/hls/syn/report/csynth.rpt"
-        if [ -f "$CSYNTH_RPT" ]; then
-          read latency_cycles latency_ns bram dsp ff lut uram issue_type < <(awk '
-          BEGIN {
-            hyphen_rows = 0;
-            reading_table = 0;
-          }
-          {
-            if ($0 ~ /\+-+\+/) {
-              hyphen_rows++;
-              if (reading_table == 1) {
-                reading_table = 0;
-                print latency_cycles, latency_ns, bram[1], dsp[1], ff[1], lut[1], uram[1], "";
-                exit;
-              }
-            }
-            if (hyphen_rows == 2 && reading_table == 0) {
-              reading_table = 1;
-              getline;
-              split($0, columns, "|");
-              latency_cycles = columns[5];
-              latency_ns = columns[6];
-              split(columns[11], bram, " ");
-              split(columns[12], dsp, " ");
-              split(columns[13], ff, " ");
-              split(columns[14], lut, " ");
-              split(columns[15], uram, " ");
-            }
-            if (hyphen_rows == 2 && reading_table == 1) {
-              split($0, columns, "|");
-              issue_type = columns[3];
-              if (issue_type ~ /II/) {
-                print latency_cycles, latency_ns, bram[1], dsp[1], ff[1], lut[1], uram[1], "II";
-                exit;
-              }
-            }
-          }
-          ' "$CSYNTH_RPT")
-
-          # Print the extracted values
-          if [ ! -z "$issue_type" ]; then
-            echo "  Issue Type: $issue_type"
-          else
-            echo "  Latency (cycles): $latency_cycles"
-            echo "  Latency (ns): $latency_ns"
-            echo "  BRAM: $bram"
-            echo "  DSP: $dsp"
-            echo "  FF: $ff"
-            echo "  LUT: $lut"
-            echo "  URAM: $uram"
-          fi
-
-          # Write the extracted values to the benchmark file
-          if ([ -z "$issue_type" ]); then
-            echo "$M,$N,$K,$latency_cycles,$latency_ns,$bram,$dsp,$ff,$lut,$uram" >>"$TEST_DIR"/../benchmarks/"$TEST_NAME".csv
-          else
-            echo "$M,$N,$K,$issue_type" >>"$TEST_DIR"/../benchmarks/"$TEST_NAME".csv
+        if [ "$PARALLEL" = true ]; then
+          run_test "$TEST_NAME" "$M:$N:$K" &
+          if (( $(jobs -r | wc -l) >= PARALLEL_JOBS )); then
+            wait -n
           fi
         else
-          echo "!! csynth.rpt not found for $TEST_NAME."
-        fi
-
-        # Remove the last two lines from the config file that we added to specify the m,n,k values
-        if tail -n 1 "$CONFIG_FILE" | grep -q "syn.cflags=-D dimK=$K -D dimM=$M -D dimN=$N"; then
-          sed -i '$d' "$CONFIG_FILE"
-        fi
-        if tail -n 1 "$CONFIG_FILE" | grep -q "tb.cflags=-D dimK=$K -D dimM=$M -D dimN=$N"; then
-          sed -i '$d' "$CONFIG_FILE"
+          run_test "$TEST_NAME" "$M:$N:$K"
         fi
 
         # Increment K
@@ -212,29 +252,9 @@ run_test() {
     done
     M=$((M * 2))
   done
-}
-
-# Create benchmarks directory if it doesn't exist, and clear it if it does
-mkdir -p "$TEST_DIR"/../benchmarks
-
-# Iterate over all the tests in the test directory
-TEST_ARRAY=($TEST_LIST)
-SIZE_ARRAY=($SIZE_LIST)
-
-if [ "$PARALLEL" = true ]; then
-  # Run tests in parallel
-  for i in "${!TEST_ARRAY[@]}"; do
-    TEST="${TEST_ARRAY[$i]}"
-    SIZE="${SIZE_ARRAY[$i]}"
-    run_test "$TEST" "$SIZE" &
-  done
-  # Wait for all parallel jobs to complete
+  echo "Waiting for all jobs to finish"
   wait
-else
-  # Run tests sequentially
-  for i in "${!TEST_ARRAY[@]}"; do
-    TEST="${TEST_ARRAY[$i]}"
-    SIZE="${SIZE_ARRAY[$i]}"
-    run_test "$TEST" "$SIZE"
-  done
-fi
+done
+
+# Remove the temp directory
+rm -rf "$WORKSPACE_DIR/temp"
